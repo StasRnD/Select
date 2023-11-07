7b9e521addde3a60bc452f5f66aaf46c52f52ff1
Stas Beresnev <beresnevrnd@gmail.com>
2023-11-07 10:43:31 +0300

    refactor: rename arguments hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,9 +1,9 @@
 import React, {useEffect} from "react";
 
-export function useOutsideClick<T extends HTMLElement>(ref: React.RefObject<T>, toggleDropdownOpen: () => void){
+export function useOutsideClick<T extends HTMLElement>(ref: React.RefObject<T>, handleClick: () => void){
     function handleOutsideClick(event: MouseEvent) {
         if (ref.current && !ref.current.contains(event.target as Node)) {
-            toggleDropdownOpen()
+            handleClick()
         }
     }
 
